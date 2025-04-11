d9fe5f31acb4fdef56644d2b89caf5bc3faff00d
Aman Kumar <103573041+amankumar-in@users.noreply.github.com>
2025-04-11 12:22:19 +0530

    jwt

--- unite-backend/config/plugins.ts
+++ unite-backend/config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET", "e8lBEkQp4BDBRUH99bChcA=="),
+    },
+  },
+});
